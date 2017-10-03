0b14e6e4795f8241452e02076d79be9603e5381e
Zhenia\ZheniaZgurovets <zhenia.zgurovets@gmail.com>
2017-10-04 01:55:32 +0300

    #Pushing all project

---
+++ Web.EASE/Web.EASE/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Web.EASE.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Web.EASE/Web.EASE/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Web.EASE.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
